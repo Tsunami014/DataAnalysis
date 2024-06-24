c5387d2f0907b4051cab19d81c22c21cc27978fb
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-06-24 16:03:17 +1000

    Tested methods of replacing things

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def main():
-    return flask.render_template("index.html")
+    return flask.render_template("index.html").replace(r'%uniqueTextToBeReplaced%%', 'THIS TEXT GOT REPLACED >:)')
+
+@app.route('/test_get')
+def test_get():
+    return "THIS TEXT ALSO GOT REPLACED >:) >:)"
 
 app.run()
