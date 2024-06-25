d3bc4a77ce7e4f3c641efe58a31fa892c6e562cf
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-06-25 16:31:33 +1000

    Made the theme selector switch work!

--- static/main.js
+++ static/main.js
@@ -8,7 +8,13 @@ function tick() {
 }
 window.onload = function() { // We initialise the variables in the onload function so it is asserted they exist in the website.
     canvas = document.getElementById("game_canvas");
-    bg = new BG_Anxiety([canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height], true);
+    bg = new BG_Anxiety([canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height]);
     ctx = canvas.getContext("2d");
     setInterval(tick, 100);
+    changeTheme(document.getElementById("themeSwitch").checked);
+}
+
+function changeTheme(newTheme) {
+    bg.whiteMode = newTheme;
+    document.body.style = 'color-scheme: ' + (newTheme ? 'light' : 'dark');
 }
